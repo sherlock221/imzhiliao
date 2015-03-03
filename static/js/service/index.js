ef8f323888e885d50dfaa1f9b0284c895e6e827c
abjia <daohao1914>
2015-03-03 18:55:21 +0800

    知了官网首次提交 模板搭建

---
+++ static/js/service/index.js
@@ -0,0 +1,42 @@
+/**
+ * 首页js
+ */
+
+
+define(function (require, exports, module) {
+
+    //暴露全局
+    window.$  = require("$");
+
+    //引入翻页
+    require("full-page")($);
+
+    var UI = {
+        $zhiliaoPage :  $("#zhiliaoPage")
+    };
+
+    var Event = {
+        init : function(){
+
+            //初始化全屏
+            UI.$zhiliaoPage.fullpage({
+
+                onLeave : function(){
+                    console.log("leave...");
+                }
+
+            });
+
+        }
+    }
+
+
+
+    var Ajax = {
+
+    }
+
+    Event.init();
+});
+
+
